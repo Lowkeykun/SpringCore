c7d089e9e473eed4a949191e11d4e1a3bdb16b19
Jerome Felicilda <felicilda980@gmail.com>
2024-09-01 14:43:44 +0800

    Understanding @Bean in spring context

--- example1/src/main/java/com/example/main/Main.java
+++ example1/src/main/java/com/example/main/Main.java
@@ -16,13 +16,15 @@ public class Main {
         // Configuration of beans
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        // getting the bean from the class
+        // getting the bean from the Vehicle class
         Vehicle moveVehicle = context.getBean(Vehicle.class);
         System.out.println("Vehicle name spring context: " + moveVehicle.getName());
 
+        // getting the bean from the String class
         String sayHello = context.getBean(String.class);
         System.out.println("String value spring context: " + sayHello);
 
+        // getting the bean from the Integer class
         Integer countNumber = context.getBean(Integer.class);
         System.out.println("Integer value spring context: " + countNumber);
 
