60aad10ccf67817fb1055659d3d9cec671de6f56
Jerome Felicilda <felicilda980@gmail.com>
2024-09-01 17:25:30 +0800

    Wiring beans using method parameters

---
+++ example10/src/main/java/com/example/main/Main.java
@@ -0,0 +1,31 @@
+package com.example.main;
+
+import com.example.config.ProjectConfig;
+import com.example.model.Person;
+import com.example.model.Vehicle;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Configuration of beans where we construct in the xml in the resources folder and we use ClassPathXmlApplicationContext
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        // gets the bean of each class
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        Person person = context.getBean(Person.class);
+
+        // after getting the bean, we can fetch the corresponding values of each beans
+        System.out.println("Hello " + person.getName());
+        System.out.printf("Your %s is looking good%n", vehicle.getName());
+        System.out.println(person.getVehicle());
+        System.out.printf("The owner of %s is %s%n", vehicle.getName(), person.getName());
+
+
+    }
+}
